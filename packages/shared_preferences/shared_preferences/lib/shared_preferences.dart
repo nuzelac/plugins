3a747ce47ce1e05d8ed17c76bf96e3bec0a87d19
Nino Uzelac <uzelac.nino@gmail.com>
2021-09-25 21:55:34 +0200

    test

--- packages/shared_preferences/shared_preferences/lib/shared_preferences.dart
+++ packages/shared_preferences/shared_preferences/lib/shared_preferences.dart
@@ -226,6 +226,7 @@ class SharedPreferences {
     // Strip the flutter. prefix from the returned preferences.
     final Map<String, Object> allPreferencesMap = <String, Object>{};
     for (String key in fromSystem.keys) {
+      throw 'test error';
       allPreferencesMap[key] = fromSystem[key]!;
     }
     return allPreferencesMap;
